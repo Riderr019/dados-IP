6eb94af7af3bdc970fda9d6382e2858c67044c2a
rider019 <154712054+Riderr019@users.noreply.github.com>
2024-01-22 23:49:54 +0000

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -2,10 +2,10 @@
 clear
 echo "Digite o endereço IP desejado"
 echo "-----------------------------"
-echo " >>> "
+echo -n " >>> "
 read ip
 curl -s ipinfo.io/$ip
 read dip
-echo "---------------------"
+echo "-----------------------------"
 echo $dip
 #
